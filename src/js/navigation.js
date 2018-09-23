65482947e05385cf46551aced88981ed5568350b
Pritak Patel <pritak.patel@gmail.com>
2018-09-22 18:06:17 -0700

    Added registry modal link in navbar

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,8 +1,17 @@
 document.getElementById("nav-toggle").addEventListener("click", toggleNav);
 document.getElementById("main-menu").addEventListener("click", toggleNav);
 document.getElementById("logo").addEventListener("click", toggleNav);
+document.getElementById("registry-click").addEventListener("click", registryModal);
+document.getElementById("modal-close").addEventListener("click", registryModal);
 
-
+function registryModal() {
+    var modal = document.getElementById("registry-modal");
+    if (modal.className == "modal") {
+        modal.className = "modal is-active";
+    } else {
+        modal.className = "modal";
+    }
+}
 
 function toggleNav() {
     var nav = document.getElementById("main-menu");
